aa03e1858c018c725c93cba488a998a2b420f1fc
Uche Chilaka <uche@lar.city>
2023-07-17 06:30:19 -0400

    component: StyledButton

---
+++ src/components/shared/StyledButton.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Button } from '@mui/material'
+import { styled } from '@mui/system'
+
+const StyledButton = styled(Button)`
+  margin-top: 16px;
+`
+
+export default StyledButton
