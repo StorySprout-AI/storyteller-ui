7db6a93107bf3c6cf42cdcd037ef5e5562f01911
Uche Chilaka <uche@lar.city>
2023-07-26 02:51:03 -0400

    SAVEPOINT

--- src/hooks/useStoryPrompt.ts
+++ src/hooks/useStoryPrompt.ts
@@ -7,7 +7,7 @@ export default function useStoryPrompt() {
   const [object, setObject] = useState('')
   const [age, setAge] = useState('')
   const [subject, setSubject] = useState('')
-  const composePrompt = useCallback(() => {
+  const composePrompt = useCallback<() => string>(() => {
     return `Write a 10 page story about ${subject} with a hero named ${hero}, \
     set in a place called ${place}, featuring a ${character} which you need to name, \
     and a significant object referred to as ${object}. The story must be age-appropriate \
