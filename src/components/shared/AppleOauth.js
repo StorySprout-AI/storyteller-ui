714b198ab584fec84e9f987d658f5ff34f15a98f
Uche Chilaka <uche@lar.city>
2023-08-09 06:05:31 -0400

    SAVEPOINT

--- src/components/shared/AppleOauth.js
+++ src/components/shared/AppleOauth.js
@@ -1,32 +1,24 @@
-import React, { useCallback } from 'react'
+import useURLFragmentParams from 'hooks/useURLFragmentParams'
+import React, { useEffect } from 'react'
 import AppleLogin from 'react-apple-login'
-import { useLocation } from 'react-router-dom'
-import queryString from 'query-string'
 
 export default function AppleOauth({ ...rest }) {
   const { onSuccess, onError } = rest
-  const location = useLocation()
-  const { code, id_token, state } = queryString.parse(location.hash)
+  const { code, id_token, state, error } = useURLFragmentParams()
 
-  console.debug({ code, id_token, state })
-
-  const handleCallback = useCallback((data) => {
-    console.debug('Apple login response', { data })
-    if (data.error) {
-      console.error(data.error)
-      onError(data)
-    } else {
-      console.info({ data })
-      onSuccess(data)
-    }
-  }, [onSuccess, onError])
+  useEffect(() => {
+    if(code && id_token && state)
+      onSuccess({ credential: id_token, code, id_token, state })
+    else if(error)
+      onError(error)
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [code, id_token, state, error])
 
   return (
     <AppleLogin 
       responseType="code id_token"
       responseMode="fragment"
       state="apple_oauth_via_fragment"
-      callback={handleCallback}
       clientId="app.storysprout.web.auth"
       redirectURI="https://storysprout.ngrok.app"
       designProp={{ width: 180, height: 40, border_radius: 10 }}
---
+++ src/hooks/useURLFragmentParams.ts
@@ -0,0 +1,20 @@
+import { useEffect, useState } from 'react'
+import { useLocation } from 'react-router-dom'
+
+export default function useURLFragmentParams() {
+  const [params, setParams] = useState<Record<string, string>>({})
+  const location = useLocation()
+
+  useEffect(() => {
+    if (location.hash) {
+      const searchParams = new URLSearchParams(location.hash.replace(/^#?/, ''))
+      let fragmentParams = {}
+      searchParams.forEach((value, key) => {
+        fragmentParams = { ...fragmentParams, [key]: value }
+      })
+      setParams(fragmentParams)  
+    }
+  }, [location.hash])
+
+  return params
+}
