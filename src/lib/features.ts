060cac62ae64f3838d10672e8568e194dc200494
Uche Chilaka <uche@lar.city>
2023-09-19 21:46:07 -0400

    refactor: feature flag a bunch of links that don't work right now

--- src/lib/features.ts
+++ src/lib/features.ts
@@ -1,5 +1,10 @@
 export const FEATURE_FLAGS: Record<string, string> = {
-  APPLE_LOGIN: 'feat__apple_login'
+  APPLE_LOGIN: 'feat__apple_login',
+  HOME_PAGE: 'feat__home_page',
+  ABOUT_PAGE: 'feat__about_page',
+  ACCOUNT: 'feat__account',
+  LIBRARY: 'feat__library',
+  PROFILE: 'feat__profile'
 }
 
 export default FEATURE_FLAGS
