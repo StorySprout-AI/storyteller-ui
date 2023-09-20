3930eb5d0d09053c06828d5cc8502e2d453acf20
Uchenna Chilaka <2763396+uchilaka@users.noreply.github.com>
2023-09-19 23:33:51 -0400

    feat: (ship ahead 1) FeatureFlagProvider (#14)
    <!--- Provide a general summary of your changes in the Title above -->
    
    ## Jira Card
    <!--- Please link the Jira card here -->
    
    [STORYAI-###](https://larcity.atlassian.net/browse/STORYAI-###)
    
    ## Summary
    <!--- Describe your changes, keeping in mind the Jira card should hold
    most of the context -->
    
    > Focus on what's important to note here. The Jira card should provide
    the rest the context.
    
    ## Screenshots (if appropriate):
    
    ## Review app(s)
    
    <!-- These should come from Vercel or Render -->
    
    - <storyteller-ui-git-###-donnadieu.vercel.app>
    
    ## Types of changes
    <!--- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to change)
    
    ## Checklist:
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    - [ ] My code follows the code style of this project.
    - [ ] My change requires a change to the README/confluence
    documentation.
    - [ ] I have updated the relevant documentation accordingly.
    - [ ] I have added tests to cover my changes.
    - [ ] All new and existing tests passed.

--- src/features/FeatureFlags/Feature.test.tsx
+++ src/features/FeatureFlags/Feature.test.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import mockAxios from 'jest-mock-axios'
 import { render, screen, waitFor } from '@testing-library/react'
 
+import { FeatureFlagProvider } from './index'
 import Feature from './Feature'
 
 // Fixing the "not wrapped in act(...)" warning: https://kentcdodds.com/blog/fix-the-not-wrapped-in-act-warning
@@ -12,7 +13,11 @@ describe('<Feature />', () => {
 
   test('renders when flag is enabled', async () => {
     mockAxios.get.mockResolvedValueOnce({ data: { features: [{ key: 'feat__hello', state: 'on' }] } })
-    render(<Feature flag="feat__hello">Hello</Feature>)
+    render(
+      <FeatureFlagProvider>
+        <Feature flag="feat__hello">Hello</Feature>
+      </FeatureFlagProvider>
+    )
     await waitFor(() => mockAxios.get)
     expect(mockAxios.get).toHaveBeenCalledTimes(1)
     await screen.findByText('Hello')
@@ -20,7 +25,11 @@ describe('<Feature />', () => {
 
   test('does not render when flag is disabled', async () => {
     mockAxios.get.mockResolvedValueOnce({ data: { features: [{ key: 'feat__hello', state: 'off' }] } })
-    render(<Feature flag="feat__hello">Hello</Feature>)
+    render(
+      <FeatureFlagProvider>
+        <Feature flag="feat__hello">Hello</Feature>
+      </FeatureFlagProvider>
+    )
     await waitFor(() => mockAxios.get)
     expect(mockAxios.get).toHaveBeenCalledTimes(1)
     expect(screen.queryByText('Hello')).not.toBeInTheDocument()
@@ -28,7 +37,11 @@ describe('<Feature />', () => {
 
   test('does not render when flag is not defined', async () => {
     mockAxios.get.mockResolvedValueOnce({ data: { features: [] } })
-    render(<Feature flag="feat__hello">Hello</Feature>)
+    render(
+      <FeatureFlagProvider>
+        <Feature flag="feat__hello">Hello</Feature>
+      </FeatureFlagProvider>
+    )
     await waitFor(() => mockAxios.get)
     expect(mockAxios.get).toHaveBeenCalledTimes(1)
     expect(screen.queryByText('Hello')).not.toBeInTheDocument()
