87c469f86c3a0adc9de9be7b12afc013951910b8
Uche Chilaka <uche@lar.city>
2023-10-14 06:29:56 -0400

    refactor: use enums for feature flags

--- src/lib/features.ts
+++ src/lib/features.ts
@@ -1,11 +1,12 @@
-export const FEATURE_FLAGS: Record<string, string> = {
-  APPLE_LOGIN: 'feat__apple_login',
-  HOME_PAGE: 'feat__home_page',
-  ABOUT_PAGE: 'feat__about_page',
-  ACCOUNT: 'feat__account',
-  LIBRARY: 'feat__library',
-  PROFILE: 'feat__profile',
-  SITE_TRANSLATION: 'feat__site_translation'
+export enum FEATURE_FLAGS {
+  APPLE_LOGIN = 'feat__apple_login',
+  HOME_PAGE = 'feat__home_page',
+  ABOUT_PAGE = 'feat__about_page',
+  ACCOUNT = 'feat__account',
+  LIBRARY = 'feat__library',
+  PROFILE = 'feat__profile',
+  SITE_TRANSLATION = 'feat__site_translation',
+  SOCIAL_LINKS = 'feat__social_links'
 }
 
 export default FEATURE_FLAGS
