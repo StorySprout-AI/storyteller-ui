97f96f746a37e65501e38a99edc1d8a5fcc5beee
Uchenna Chilaka <2763396+uchilaka@users.noreply.github.com>
2023-10-19 09:13:05 -0400

    refactor: feature flag enums (#23)
    * refactor: wrap site translation in a feature flag
    
    * refactor: use enums for feature flags
    
    * refactor: social links behind a feature flag

--- src/lib/features.ts
+++ src/lib/features.ts
@@ -1,12 +1,12 @@
-export const FEATURE_FLAGS: Record<string, string> = {
-  APPLE_LOGIN: 'feat__apple_login',
-  HOME_PAGE: 'feat__home_page',
-  ABOUT_PAGE: 'feat__about_page',
-  ACCOUNT: 'feat__account',
-  LIBRARY: 'feat__library',
-  PROFILE: 'feat__profile',
-  SITE_TRANSLATION: 'feat__site_translation',
-  SOCIAL_LINKS: 'feat__social_links'
+export enum FEATURE_FLAGS {
+  APPLE_LOGIN = 'feat__apple_login',
+  HOME_PAGE = 'feat__home_page',
+  ABOUT_PAGE = 'feat__about_page',
+  ACCOUNT = 'feat__account',
+  LIBRARY = 'feat__library',
+  PROFILE = 'feat__profile',
+  SITE_TRANSLATION = 'feat__site_translation',
+  SOCIAL_LINKS = 'feat__social_links'
 }
 
 export default FEATURE_FLAGS
